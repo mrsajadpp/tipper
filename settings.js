8d4bab76b3450ee17d25f73ca93559a6800ffa8f
Jay M Angélil <96676224+sajadpp120@users.noreply.github.com>
2022-01-15 06:51:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,3 +5,8 @@ window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
 }
+const menu = document.querySelector('#menu');
+const sidebar = document.querySelector('.sidebar');
+menu.addEventListener('click', function() {
+    sidebar.classList.toggle('show-sidebar');
+})
